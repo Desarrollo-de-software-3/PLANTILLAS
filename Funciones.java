253097a29bf239bff092b22448eae99a2ac66d6e
Javier Acosta Tullock <javier1009rm@gmail.com>
2023-06-02 20:03:19 -0500

    Update Funciones.java

--- Funciones.java
+++ Funciones.java
@@ -1,56 +1,20 @@
-import java.io.*;
+public class Registro {
+    private String matricula;
+    private String centroR;
 
-// Nombre del archivo
-class Funciones extends Archivos{
-    private String placa;
-    private String centroRegional;
-    private PrintWriter s = getWriter();
-
-    public Funciones() throws Exception {
-        super();
+    public String getCentroR() {
+        return centroR;
     }
 
-    public void asignar(String placa, String centroRegional) {
-        this.placa = placa;
-        this.centroRegional = centroRegional; 
+    public void setCentroR(String centroR) {
+        this.centroR = centroR;
     }
 
-    // Se guarda cada dato en una línea nueva
-    public void grabar() throws Exception {
-        try {
-            s.println(placa + ";" + centroRegional + ";");
-        } catch (Exception e) {
-            throw new Exception("Error al grabar los datos: " + e.getMessage());
-        }
+    public String getMatricula() {
+        return matricula;
     }
 
-
-    public boolean buscarPlaca(String placa) throws Exception {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(getArchivo()));
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] campos = linea.split(";");
-                if (campos.length >= 1 && campos[0].equals(placa)) {
-                    return true; // La placa se encontró en el archivo
-                }
-            }
-            return false; // La placa no se encontró en el archivo
-        } catch (IOException e) {
-            throw new Exception("Error al buscar la placa: " + e.getMessage());
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    throw new Exception("Error al cerrar el archivo: " + e.getMessage());
-                }
-            }
-        }
-    }
-
-    public String getPlaca() {
-        return placa;
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
     }
 }
